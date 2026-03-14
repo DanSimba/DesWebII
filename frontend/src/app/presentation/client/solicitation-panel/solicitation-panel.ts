9d5174ce4b86b89173dd06917429815ebefb4789
Roberto Rigo <robertorigo18@gmail.com>
2026-03-14 18:59:48 -0300

    MOD: estiliza e completa solicitation panel

--- frontend/src/app/presentation/client/solicitation-panel/solicitation-panel.ts
+++ frontend/src/app/presentation/client/solicitation-panel/solicitation-panel.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SolicitationClient } from '../solicitation-client/solicitation-card';
+import { Solicitation } from '../../../models/solicitation-interface';
 
 @Component({
   selector: 'app-solicitation-panel',
@@ -8,5 +9,33 @@ import { SolicitationClient } from '../solicitation-client/solicitation-card';
   styleUrl: './solicitation-panel.css',
 })
 export class SolicitationPanel {
+  date = new Date(2025, 5, 24, 10, 30)
 
+  sols: Solicitation[]= [
+    {
+        id: "1",
+        nomeCli: "RAZER",
+        equipamento: "compiuter",
+        dataHora: `${this.date.getDate()}/${this.date.getMonth()}/${this.date.getFullYear()}`,
+        estado: "Pendente",
+        desc: "moiado"
+    },
+    {
+        id: "2",
+        nomeCli: "RAFINHA",
+        equipamento: "cerula",
+        dataHora: `${this.date.getDate()}/${this.date.getMonth()}/${this.date.getFullYear()}`,
+        estado: "Concluída",
+        desc: "deu pau"
+    },
+    {
+        id: "3",
+        nomeCli: "PEDRINHO",
+        equipamento: "tablet",
+        dataHora: `${this.date.getDate()}/${this.date.getMonth()}/${this.date.getFullYear()}`,
+        estado: "Concluída",
+        desc: "quebrado"
+    },
+    
+  ]
 }
