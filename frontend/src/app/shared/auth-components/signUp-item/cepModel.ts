291f5c88f07d2ca3e7476118f28473108f8ff3f1
Gerson Belniowski Junior <gersonbeljr@gmail.com>
2026-03-15 20:35:43 -0300

    [MOD] Iniciado Usando ViaCEP

---
+++ frontend/src/app/shared/auth-components/signUp-item/cepModel.ts
@@ -0,0 +1,3 @@
+export class CepModel{
+    cep: String = '';
+}
--- frontend/src/app/shared/auth-components/signUp-item/signUp-Component.ts
+++ frontend/src/app/shared/auth-components/signUp-item/signUp-Component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CepType } from '../../../services/CEP/cep.type';
+import { CepService } from '../../../services/CEP/cep.service';
+import { CepModel } from './cepModel';
+import { ReactiveFormsModule, FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signUpComponent',
-  imports: [MatIconModule],
+  imports: [MatIconModule, ReactiveFormsModule, CommonModule],
   standalone: true,
-  templateUrl: './signUp-Component.html'
+  templateUrl: './signUp-Component.html',
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  formCep: FormGroup;
+  submittedCepForm: any;
+  cepResponse: CepType = {
+    CEP: '',
+    Logradouro: '',
+    UF: '',
+    Bairro: '',
+    Complemento: '',
+  };
 
+  constructor(private readonly cepService: CepService, private formBuilder: FormBuilder) {
+    this.formCep = this.formBuilder.group({
+      cep: new FormControl('')
+    });
+  }
+
+  ngOnInit(): void {
+    this.createFormCep(new CepModel());
+  }
+
+  createFormCep(cepModel: CepModel){
+    this.formCep = this.formBuilder.group({
+      cep: [cepModel.cep]
+    });
+  }
+
+  onSubmitCepForm(){
+    this.submittedCepForm = this.formCep.value;
+    let cep = this.submittedCepForm.cep;
+    cep = cep.replace(/\D/g, '');
+    try {
+      this.cepService.getCep(cep).subscribe((response)=>{
+        this.cepResponse = response; 
+      })
+    } catch (error) {
+      
+    }
+  }
 }
