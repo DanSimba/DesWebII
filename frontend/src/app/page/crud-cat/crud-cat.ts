16dc05e244e9083a841d14a8868924cc94d074a6
Ender Ruiz <ender.ruiz@ufpr.br>
2026-03-16 01:10:26 -0300

    [MOD] visualização de crud categorias

--- frontend/src/app/page/crud-cat/crud-cat.ts
+++ frontend/src/app/page/crud-cat/crud-cat.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBarFunc } from '../../shared/components/nav-bar-func/nav-bar-func';
+import { CategoriaCrud } from "../../presentation/func/categoria-crud/categoria-crud";
 
 @Component({
   selector: 'app-crud-cat',
-  imports: [],
+  imports: [RouterOutlet, NavBarFunc, CategoriaCrud],
   templateUrl: './crud-cat.html',
   styleUrl: './crud-cat.css',
 })
