c4371b74166ddb7494bc2d1d0b36a9ec16852fe0
UilianRafaelGon <uilianrafaelgon@gmail.com>
2026-03-17 00:01:35 -0300

    [MOD]Ajuste nas rotas de funcionário

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeView } from './page/home-view/home-view';
-import { MaintenanceListView } from './page/maintenance-list-view/maintenance-list-view';
 import { Client } from './page/client/client';
 import { SolicitationFormClient } from './presentation/client/solicitation-form-client/solicitation-form-client';
 import { Auth } from './page/auth/auth';
 import { LoginComponent } from './shared/auth-components/login-item/login-component';
 import { SignUpComponent } from './shared/auth-components/signUp-item/signUp-Component';
-import { CrudCat } from './page/crud-cat/crud-cat';
 import { Func } from './page/func/func';
 
 export const routes: Routes = [
@@ -21,10 +19,6 @@ export const routes: Routes = [
             { path: 'login', component: LoginComponent },
             { path: 'signup', component: SignUpComponent }
         ]
-    },
-    {
-        path: 'maintenance-list',
-        component: MaintenanceListView
     },
     {
         path: 'client',
@@ -48,11 +42,18 @@ export const routes: Routes = [
         path: 'employee',
         component: Func,
         children: [
-            {
+{
                 path: 'panel',
                 loadComponent: () => import('./presentation/func/maintenance-panel/maintenance-panel').then(m => m.MaintenancePanel)
             },
-            { path: 'crud-cat', component: CrudCat }
+            {
+                path: 'list',
+                loadComponent: () => import('./page/maintenance-list-view/maintenance-list-view').then(m => m.MaintenanceListView)
+            },
+            { 
+                path: 'crud-cat', 
+                loadComponent: () => import('./page/crud-cat/crud-cat').then(m => m.CrudCat) 
+            }
         ]
     }
 ];
