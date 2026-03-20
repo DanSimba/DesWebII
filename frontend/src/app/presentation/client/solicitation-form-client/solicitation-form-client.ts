aec0bf8f67e0c179e83a71ec4fe3182ce90db57e
DanSimba <danteksimba@gmail.com>
2026-03-19 23:17:22 -0300

    [ADD] adiciona componente solicitation-form-client

--- frontend/src/app/presentation/client/solicitation-form-client/solicitation-form-client.ts
+++ frontend/src/app/presentation/client/solicitation-form-client/solicitation-form-client.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-solicitation-form-client',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule,
+    MatInputModule, MatSelectModule, MatButtonModule],
   templateUrl: './solicitation-form-client.html',
   styleUrl: './solicitation-form-client.css',
 })
 export class SolicitationFormClient {
+  form! : FormGroup;  
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      descEquip:  ['', [Validators.required, Validators.maxLength(125)]], //metade de um tweet deve ser o suficiente pra dizer modelo do rolê
+      catID:  ['', Validators.required],
+      descDefeito:  ['', [Validators.required, Validators.maxLength(255)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched(); 
+      return;
+    }
+    // em algum momento isso daqui vai ir pro back, confia !!!!
+      console.log(this.form); 
+  }
 
 }
