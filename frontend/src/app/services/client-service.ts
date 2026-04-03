8add036d863bedd550dbef9b65f2c56236b7360c
Roberto Rigo <robertorigo18@gmail.com>
2026-04-03 19:54:17 -0300

    feat: ligação entre SolicitationFormClient e SolicitationPanel

--- frontend/src/app/services/client-service.ts
+++ frontend/src/app/services/client-service.ts
@@ -1,6 +1,6 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable,of } from 'rxjs';
 import { Solicitation } from '../models/solicitation-interface';
 import { ClientInterface } from '../models/client-interface';
 
@@ -10,16 +10,19 @@ import { ClientInterface } from '../models/client-interface';
 export class ClientService {
     private apiUrl = 'END-BACKEND';
     private jsonUrl = 'assets/client-ex.json'; //VERSÃO COM JSON (CLIENTE DE EXEMPLO)
+    public sols = signal<Solicitation[]>([]); //para guardar as sols criadas no solicitation-form
     constructor(private http: HttpClient) {}
 
-    getSols(id:string): Observable <Solicitation[]>{ //retorna tds as sols do cliente 
-      return this.http.get<Solicitation[]>(`${this.apiUrl}/clients/${id}/sols`);
+    getSols(): Observable <Solicitation[]>{ //REWORK: essa função agr retorna a lista sols
+      return of(this.sols()); 
     }
-
-    //OU
 
     getClient(id: string): Observable<ClientInterface> { //retorna diretamente o cliente
       //return this.http.get<ClientInterface>(`${this.apiUrl}/clients/${id}`); //VERSÃO QUE BUSCA DO BACK, PARA A SEGUNDA PARTE
       return this.http.get<ClientInterface>(this.jsonUrl);
     }
+
+  addSol(newSol: Solicitation) { //adiciona a lista de sols que o cliente criou
+    this.sols.update(current => [...current, newSol]);
+  }
 }
