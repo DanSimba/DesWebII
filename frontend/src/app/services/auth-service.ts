2849e230d5850cc26b10ad1d2fcecd5cd453ffd1
Roberto Rigo <robertorigo18@gmail.com>
2026-05-14 00:20:43 -0300

    feat: guard do cliente funcionando e alteração da função login no auth service

--- frontend/src/app/services/auth-service.ts
+++ frontend/src/app/services/auth-service.ts
@@ -1,7 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from '../models/usr-interface';
-import { Observable } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+import { Observable, map } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,16 @@ export class AuthService {
   private http = inject(HttpClient);
   private readonly apiURL = 'http://localhost:8080/auth';
 
-  login(email: string, senha: string) : Observable<{token: string}>{
-    return this.http.post<{token: string}>(`${this.apiURL}/login`, { email, senha });
+  private token = 'token';
+
+  login(email: string, senha: string): Observable<any>{
+    return this.http.post<{token: string}>(`${this.apiURL}/login`, { email, senha })
+      .pipe(
+            map(response => {
+              this.salvarToken(response.token);
+              return response.token;
+            })
+          )
   }
 
   // aqui para auto cadastro
@@ -20,7 +29,22 @@ export class AuthService {
   }
 
   salvarToken(token: string): void {
-    localStorage.setItem('token', token);
+    localStorage.setItem(this.token, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.token);
+  }
+
+  isAuth(): boolean {
+    const t = this.getToken();
+    if(!t)return false;
+
+    const decoded: any = jwtDecode(t); //n sei se vai dar certo mas here we go
+    const now = Date.now()/1000;
+
+    if(decoded.exp>now) return true; //exp = expiração
+    return false;
   }
 
   logout(): void {
--- frontend/src/app/shared/guards/client.guard.ts
+++ frontend/src/app/shared/guards/client.guard.ts
@@ -1,9 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
+import { Router } from '@angular/router';
 
 export const clientGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
-  //if( )
-  return true;
+  const router = inject(Router);
+  if(authService.isAuth())return true;
+
+  router.navigate(['auth/login']);
+  return false;
 };
