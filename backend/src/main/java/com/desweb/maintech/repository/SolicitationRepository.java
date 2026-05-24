c237b38e98616cd86cb7b5060f80f4b66f8f3503
UilianRafaelGon <uilianrafaelgon@gmail.com>
2026-05-23 21:02:29 -0300

    A

--- backend/src/main/java/com/desweb/maintech/repository/SolicitationRepository.java
+++ backend/src/main/java/com/desweb/maintech/repository/SolicitationRepository.java
@@ -4,9 +4,12 @@ import com.desweb.maintech.entity.Solicitation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface SolicitationRepository extends JpaRepository<Solicitation, Long> {
     List<Solicitation> findByClientId(Long clientId);
+
+    List<Solicitation> findByDataHoraBetween(LocalDateTime dataInicio, LocalDateTime dataFim);
 }
