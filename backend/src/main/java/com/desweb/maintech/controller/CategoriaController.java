70d558a8ec187fe6424026039c015b71fe9e9b37
Ender Ruiz <ender.ruiz@ufpr.br>
2026-05-29 10:11:00 -0300

    [MOD] pequenos ajustes

--- backend/src/main/java/com/desweb/maintech/controller/CategoriaController.java
+++ backend/src/main/java/com/desweb/maintech/controller/CategoriaController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.desweb.maintech.dto.CategoriaDTO;
 import com.desweb.maintech.service.CategoriaService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/api/categorias")
 @CrossOrigin(origins = "*")
@@ -28,27 +30,32 @@ public class CategoriaController {
         this.service = service;
     }
 
+    @Operation(summary = "Lista todas as categorias")
     @GetMapping
     public ResponseEntity<List<CategoriaDTO>> listarTodas() {
         return ResponseEntity.ok(service.listarTodas());
     }
 
+    @Operation(summary = "Busca categoria pelo Id")
     @GetMapping("/{id}")
     public ResponseEntity<CategoriaDTO> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(service.buscarPorId(id));
     }
 
+    @Operation(summary = "Salva uma nova categoria")
     @PostMapping
     public ResponseEntity<CategoriaDTO> criar(@RequestBody CategoriaDTO dto) {
         CategoriaDTO novaCategoria = service.salvar(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaCategoria);
     }
 
+    @Operation(summary = "Atualiza uma categoria")
     @PutMapping("/{id}")
     public ResponseEntity<CategoriaDTO> atualizar(@PathVariable Long id, @RequestBody CategoriaDTO dto) {
         return ResponseEntity.ok(service.atualizar(id, dto));
     }
 
+    @Operation(summary = "Remove uma categoria")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> remover(@PathVariable Long id) {
         service.desativar(id);
