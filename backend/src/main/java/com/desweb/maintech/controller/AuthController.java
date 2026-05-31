afff7197cde0cdc0ecac92d0777b3453c009c120
Gerson Belniowski Junior <gersonbeljr@gmail.com>
2026-05-30 23:49:32 -0300

    [FIX] Cadastro fora do AuthControler

--- backend/src/main/java/com/desweb/maintech/controller/AuthController.java
+++ backend/src/main/java/com/desweb/maintech/controller/AuthController.java
@@ -2,21 +2,27 @@ package com.desweb.maintech.controller;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.desweb.maintech.dto.ClientDTO;
 import com.desweb.maintech.service.AuthService;
+import com.desweb.maintech.service.ClientService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService service;
+    private final ClientService clientService;
 
-    public AuthController(AuthService s) {
+    public AuthController(AuthService s, ClientService clientService) {
         this.service = s;
+        this.clientService = clientService;
     }
 
     @PostMapping("/login")
@@ -27,5 +33,11 @@ public class AuthController {
         );
 
         return Map.of("token", token);
+    }
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<ClientDTO> criar(@RequestBody ClientDTO dto) {
+        ClientDTO novo = clientService.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novo);
     }
 }
